1b99975852f46a5dd3957b53b8eab0d231386f29
Jamshed Singh Gill <tnk577@alumni.ku.dk>
2018-05-19 21:38:48 +0200

    Update how_to_open_file_script.R

--- how_to_open_file_script.R
+++ how_to_open_file_script.R
@@ -2,9 +2,8 @@ getwd()
 setwd("H:/Master_Thesis/Datasets/study1_naive_diff")
 library(readxl)
 
-# file <- read.csv(file = "metadata.csv", header = TRUE, sep = ",")
-# file
-# file2 <- read_excel(path = "metadata_naiverats_envigo_without_semi.xlsx", sheet = 1, col_names = TRUE)
+file <- read.csv(file = "metadata.csv", header = TRUE, sep = ",")
+file2 <- read_excel(path = "metadata_naiverats_envigo_without_semi.xlsx", sheet = 1, col_names = TRUE)
 
 filelist = list.files(pattern = ".*.txt")
 datalist = lapply(filelist, function(x)read.table(x, header=F))
